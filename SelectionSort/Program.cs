1b186f332b855cb44766c1a93ce7fa77d6774c6e
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-05-12 19:30:51 +0530

    Selection Sort Code

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,51 @@ namespace SelectionSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Insert the elements to sort: ");
+            string[] k = (Console.ReadLine().Split(' '));
+            int[] array = new int[k.Length];
+            for (int i = 0; i < k.Length; i++)
+            {
+                array[i] = int.Parse(k[i]);
+            }
+
+            Console.WriteLine("Inserted  Array: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            //Selection sort
+            for (int i = 0; i < array.Length; i++)
+            {
+                int sindex = i;
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[j] < array[sindex])
+                    {
+                        sindex = j;
+                    }
+                }
+                int temp = array[i];
+                array[i] = array[sindex];
+                array[sindex] = temp;
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Time Taken" + stopwatch.Elapsed);
+
+            Console.WriteLine("The sorted array is: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
             Console.ReadKey();
         }
     }
